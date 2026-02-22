0e20ac1e2797c02ea3691992656986bdb699d93f
Zia-Ul-Islam <ziaulislam.tech@gmail.com>
2026-02-22 14:19:35 +0500

    post,fetch,patch and delet apis are created

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -17,6 +17,38 @@ app.post("/api/notes",async(req,res)=>{
         note
     })
 })
-//
 
+// Get Method 
+
+app.get("/api/notes",async (req,res)=>{
+    const notes = await noteModel.find()
+    res.status(200).json({
+        message : "notes fetched successfully",
+        notes
+    })
+})
+
+// Delete Method 
+app.delete("/api/notes/:id",async(req,res)=>{
+    const id = req.params.id
+    const note = await noteModel.findById(id)
+    await noteModel.findByIdAndDelete(id)
+    res.status(200).json({
+        message : "note delected successfully",
+        note
+    })
+})
+
+// Patch Method
+app.patch("/api/notes/:id",async(req,res)=>{
+    const id = req.params.id
+    const {title,description,image} = req.body
+    const note = await noteModel.findById(id) 
+     await noteModel.findByIdAndUpdate(id,{title,description,image})
+
+    res.status(200).json({
+        message : "note updated successfully",
+        note
+    })
+})
 module.exports = app
