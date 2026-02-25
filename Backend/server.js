6b149338b80f6c70a9a409b5adc330f46e06c158
Zia-Ul-Islam <ziaulislam.tech@gmail.com>
2026-02-25 07:03:19 +0500

    issues fixed for render deployment

--- Backend/server.js
+++ Backend/server.js
@@ -1,11 +1,8 @@
-// const app = require("./src/app")
-// const connectToDb = require("./src/config/database")
-// require("dotenv").config()
-// app.listen(3000,()=>{
-//     console.log("server is running at port 3000")
-// })
-// connectToDb()
+const app = require("./src/app")
+const connectToDb = require("./src/config/database")
+require("dotenv").config()
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, ()=>{
     console.log(`Server is running  on port ${PORT}`)
 })
+connectToDb()
