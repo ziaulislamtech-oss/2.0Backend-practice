e767d6d51dba53cdbb6add559ae0326baed3b06b
Zia-Ul-Islam <ziaulislam.tech@gmail.com>
2026-02-25 06:25:02 +0500

    daynamic port added for render

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,11 @@
-const app = require("./src/app")
-const connectToDb = require("./src/config/database")
-require("dotenv").config()
-app.listen(3000,()=>{
-    console.log("server is running at port 3000")
+// const app = require("./src/app")
+// const connectToDb = require("./src/config/database")
+// require("dotenv").config()
+// app.listen(3000,()=>{
+//     console.log("server is running at port 3000")
+// })
+// connectToDb()
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, ()=>{
+    console.log(`Server is running  on port ${PORT}`)
 })
-connectToDb()
